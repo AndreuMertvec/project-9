bf866899341f0104a3f0096d1a417b229130b5ca
tokar <kirill.tokar2014@yandex.ru>
2022-12-07 17:36:05 +0300

    installation JS

---
+++ massiv.js
@@ -0,0 +1,10 @@
+var numbers = [2, 3, 5, 7, 11];		// Массив с пятью числовыми элементами
+    misc = [ 1.1, true, "a", ];		// 3 элемента разных типов + завершающая запятая
+    base = 1024;
+    table = [base, base+1, base+2, base+3];		// Массив с переменными
+    arrObj = [[1,{x:1, y:2}], [2, {x:3, y:4}]];	// 2 массива внутри, содержащие объекты
+console.log(numbers);
+console.log(misc);
+console.log(base);
+console.log(table);
+console.log(arrObj);
